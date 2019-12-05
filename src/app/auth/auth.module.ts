706f441ccb80c2ee168727c4d46247b7d967b567
s.ranjgar <shabnam.ranjgar@live.com>
2019-12-05 21:00:48 +0330

    lazy loading RecipesModule

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations:[
+        AuthComponent
+    ],
+    imports:[
+        RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+        SharedModule,
+        FormsModule
+    ]
+})
+export class AuthModule{
+
+}
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ],
+    imports: [
+        RouterModule.forChild([{ path: 'shopping-list', component: ShoppingListComponent }]),
+        SharedModule, FormsModule
+    ]
+})
+export class ShoppingListModule {
+
+}
